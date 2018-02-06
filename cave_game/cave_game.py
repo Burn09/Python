1cfd56da27aaae34b21ffa6b77cfe7c2415a919a
Burn09 <taneev91@gmail.com>
2018-02-06 10:47:52 -0500

    Add files via upload

---
+++ cave_game/cave_game.py
@@ -0,0 +1,41 @@
+
+# cave_initialise.py creates the two shelves (locations and vocabulary)
+# with the appropriate keys and values.
+# cave_game.py will then use the two shelves
+
+
+import shelve
+
+locations = shelve.open("locations")
+vocabulary = shelve.open("vocabulary")
+
+loc = '1'
+while True:
+    availableExits = ", ".join(locations[loc]["exits"].keys())
+
+    print(locations[loc]["desc"])
+
+    if loc == '0':
+        break
+    else:
+        allExits = locations[loc]["exits"].copy()
+        allExits.update(locations[loc]["namedExits"])
+
+    direction = input("Available exits are " + availableExits).upper()
+    print()
+
+    # Parse the user input using vocabulary dictionary if necessary
+    if len(direction) > 1:  # more than 1 letter, so check vocab
+        words = direction.split()
+        for word in words:
+            if word in vocabulary:  # does it contain a word we know?
+                direction = vocabulary[word]
+                break
+
+    if direction in allExits:
+        loc = allExits[direction]
+    else:
+        print("You cannot go in that direction")
+
+locations.close()
+vocabulary.close()
